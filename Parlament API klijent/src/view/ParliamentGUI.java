9d6c2f412a721af22b0e4b5f5db97079662a4d44
ljuba95 <ljuba95@hotmail.com>
2016-05-13 10:21:46 +0200

    Finished GUI

--- Parlament API klijent/src/view/ParliamentGUI.java
+++ Parlament API klijent/src/view/ParliamentGUI.java
@@ -6,10 +6,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextArea;
 
 public class ParliamentGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel panel;
+	private JButton btnGetMembers;
+	private JButton btnFillTable;
+	private JButton btnUpdate;
+	private JScrollPane scrollPane;
+	private JTable table;
+	private JPanel panel_1;
+	private JScrollPane scrollPane_1;
+	private JTextArea txtStatus;
 
 	
 
@@ -18,11 +35,80 @@ public class ParliamentGUI extends JFrame {
 	 */
 	public ParliamentGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 737, 384);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(getPanel(), BorderLayout.EAST);
+		contentPane.add(getScrollPane(), BorderLayout.CENTER);
+		contentPane.add(getPanel_1(), BorderLayout.SOUTH);
 	}
-
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setPreferredSize(new Dimension(140, 100));
+			panel.add(getBtnGetMembers());
+			panel.add(getBtnFillTable());
+			panel.add(getBtnUpdate());
+		}
+		return panel;
+	}
+	private JButton getBtnGetMembers() {
+		if (btnGetMembers == null) {
+			btnGetMembers = new JButton("GET Members");
+			btnGetMembers.setPreferredSize(new Dimension(135, 30));
+		}
+		return btnGetMembers;
+	}
+	private JButton getBtnFillTable() {
+		if (btnFillTable == null) {
+			btnFillTable = new JButton("Fill table");
+			btnFillTable.setPreferredSize(new Dimension(135, 30));
+		}
+		return btnFillTable;
+	}
+	private JButton getBtnUpdate() {
+		if (btnUpdate == null) {
+			btnUpdate = new JButton("Update members");
+			btnUpdate.setPreferredSize(new Dimension(135, 30));
+		}
+		return btnUpdate;
+	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setViewportView(getTable_1());
+		}
+		return scrollPane;
+	}
+	private JTable getTable_1() {
+		if (table == null) {
+			table = new JTable();
+		}
+		return table;
+	}
+	private JPanel getPanel_1() {
+		if (panel_1 == null) {
+			panel_1 = new JPanel();
+			panel_1.setPreferredSize(new Dimension(10, 80));
+			panel_1.setBorder(new TitledBorder(null, "STATUS", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panel_1.setLayout(new GridLayout(1, 0, 0, 0));
+			panel_1.add(getScrollPane_1());
+		}
+		return panel_1;
+	}
+	private JScrollPane getScrollPane_1() {
+		if (scrollPane_1 == null) {
+			scrollPane_1 = new JScrollPane();
+			scrollPane_1.setViewportView(getTxtStatus());
+		}
+		return scrollPane_1;
+	}
+	private JTextArea getTxtStatus() {
+		if (txtStatus == null) {
+			txtStatus = new JTextArea();
+		}
+		return txtStatus;
+	}
 }
